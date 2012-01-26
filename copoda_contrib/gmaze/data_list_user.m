8303bd061ff404b9335bfe8c1fb28dcff3b6ebca
maze.guillaume <maze.guillaume@904b3b22-837f-9646-09c2-551967d61ab2>
2012-01-26 14:03:59 +0000

    Added gmaze user defined variables in the contrib folder

---
+++ copoda_contrib/gmaze/data_list_user.m
@@ -0,0 +1,58 @@
+% data_list_user Define my data
+%
+% No inputs, this function is called by the transect class to allow for additional
+% variables in the transect.data object defined by user.
+%
+% Created: 2012-01-26.
+% http://code.google.com/p/copoda
+% Copyright 2012, COPODA
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+% Category for documentation:
+%CAT 
+% Method's type for documentation:
+%TYP
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function T = data_list_user(varargin)
+
+	% TIPE stands for TRSP_INV_PLUS_EK:  Geostrophic transport plus Ekman		
+	T.TIPE = odata('name','TIPE','long_name','Absolute transport (Geostrophic+Ekman)','unit','m3/s','long_unit','m3/s');
+	T.TPOT = odata('name','TPOT','long_name','Potential Temperature','unit','degC','long_unit','degree Celsius');
+	T.VORP = odata('name','VORP','long_name','Planetary Vorticity (f/h)','unit','1/s','long_unit','1/s');
+	T.MLD  = odata('name','MLD','long_name','Mixed Layer Depth','unit','m','long_unit','meter');
+
+	% About the thermocline:
+	T.THD     = odata('name','THD'   ,'long_name','Main Thermocline Depth','unit','m','long_unit','meter');
+	T.THDTOP  = odata('name','THDtop','long_name','Main Thermocline Top Depth','unit','m','long_unit','meter');
+	T.THDBTO  = odata('name','THDbto','long_name','Main Thermocline Bottom Depth','unit','m','long_unit','meter');
+	T.THH     = odata('name','THH'   ,'long_name','Main Thermocline Thickness','unit','m','long_unit','meter');
+	T.THSIG0  = odata('name','THSIG0','long_name','Main Thermocline Potential Density','unit','kg/m3','long_unit','kg/m3');
+	T.THTEMP  = odata('name','THTEMP','long_name','Main Thermocline Temperature','unit','degC','long_unit','degree Celsius');
+	T.THPSAL  = odata('name','THPSAL','long_name','Main Thermocline Salinity','unit','PSU','long_unit','P.S.U.');
+	T.THBFRQ  = odata('name','THBFRQ','long_name','Main Thermocline Brunt-Vaisala Frequency squared','unit','1/s2','long_unit','1/s2');
+	T.THPLPV  = odata('name','THPLPV','long_name','Main Thermocline Planetary Potential Vorticity (q = f*N2/g)','unit','1/m/s','long_unit','1/m/s');
+	T.THMWD   = odata('name','THMWD' ,'long_name','Mode Water Core Depth from TH diagnostic','unit','m','long_unit','meter');
+
+	% Misc:
+	T.NOP = odata('name','NOP','long_name','Preformed Nitrate','unit','mumol/kg','long_unit','micromol/kilogram');
+
+end %functiondata_list_user
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
